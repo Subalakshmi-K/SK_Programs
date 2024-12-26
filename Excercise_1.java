322cfb574a173c0a166ee6c629808f796912c1d4
Subalakshmi K <Suba081996@gmail.com>
2024-12-26 08:29:41 +0530

    Excercise_1.java

--- Excercise_1.java
+++ Excercise_1.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s= new Scanner(System.in);
+	    
+		int a = s.nextInt();
+		String b = s.next();
+		char c = s.next().charAt(0);
+	
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		s.close();
+	}
+}
 
